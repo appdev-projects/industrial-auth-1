77ff8453d31644ae272054cb89180176f7e6aee5
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-10-27 20:19:04 +0000

    Created photo policy

--- app/policies/photo_policy.rb
+++ app/policies/photo_policy.rb
@@ -7,4 +7,9 @@ class PhotoPolicy
     @photo = photo
   end 
 
+  def show? 
+ #photo can only be seen by the owner or followers unless the owner is not private 
+    user == photo.owner || !photo.owner.private? || photo.owner.followers.include?(user)
+  end 
+
 end 
