19547909d5de93401a4a79dd02af53f2d2ff8d51
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-10-27 20:24:44 +0000

    Using before action to ensure user is auth

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,7 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
+  before_action :ensure_user_is_authorized, only: [:show]
 
   def ensure_current_user_is_owner
     if current_user != @photo.owner
@@ -8,6 +9,11 @@ class PhotosController < ApplicationController
     end
   end
 
+  def ensure_user_is_authorized 
+    if !PhotoPolicy.new(current_user, @photo).show? 
+      redirect_back fallback_location: root_url
+    end 
+  end 
   # GET /photos or /photos.json
   def index
     @photos = Photo.all
