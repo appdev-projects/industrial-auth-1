5f1c9ef8f570b187747323b26bc03b25ee2cb708
Amanda Mendez <amandaag39@gmail.com>
2024-03-11 16:32:08 -0500

    added bones for likepolicy

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,5 +1,27 @@
 # app/policies/comment_policy.rb
 
-class CommentPolicy
-  
+class CommentPolicy < ApplicationPolicy
+  attr_reader :user, :comment
+
+  def initialize(user, comment)
+    @user = user
+    @comment = comment
+  end
+
+  def show?
+    user == comment.author
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    user == comment.author
+  end
+
+  def destroy?
+    user == comment.author
+  end
+
 end
