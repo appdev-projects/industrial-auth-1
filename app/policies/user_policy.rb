83e16e373925d2eae8d879b18c5ca632678601cf
Amanda Mendez <amandaag39@gmail.com>
2024-03-12 21:12:45 -0500

    refined logic for hiding private user content

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -25,7 +25,8 @@ class UserPolicy < ApplicationPolicy
   end
 
   def view_private_profile_content?
-    user.private? && user != current_user
+    Rails.logger.debug "Private: #{user.private?}, Current User: #{current_user.id}, User: #{user.id}, Followers: #{user.followers.map(&:id)}"
+    !user.private? || user == current_user || user.followers.include?(current_user)
   end
 
   def see_follow_request_button?
